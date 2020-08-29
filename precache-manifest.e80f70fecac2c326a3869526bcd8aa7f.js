342ce30f6b6a6b609d6bac25381ce03a24d6eb09
= <badal.mishra@edeltaes.com>
2020-08-29 08:45:42 +0530

    Updates

---
+++ precache-manifest.e80f70fecac2c326a3869526bcd8aa7f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b1b84514c93d17da033f253615527d3",
+    "url": "/costing-note-app/index.html"
+  },
+  {
+    "revision": "57ac4a0a64625c4bab96",
+    "url": "/costing-note-app/static/css/main.d4c2fb1a.chunk.css"
+  },
+  {
+    "revision": "d8d75047de1e955b4576",
+    "url": "/costing-note-app/static/js/2.53b85e01.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/costing-note-app/static/js/2.53b85e01.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "57ac4a0a64625c4bab96",
+    "url": "/costing-note-app/static/js/main.9f3bb171.chunk.js"
+  },
+  {
+    "revision": "813899e2feaa6c43ef47",
+    "url": "/costing-note-app/static/js/runtime-main.6cd5defc.js"
+  }
+]);
